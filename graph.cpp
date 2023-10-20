bb38c849eada2e4c2a1b0f0ff480e3ca9937076d
Eric Rodriguez <erod_9@utexas.edu>
2023-10-20 18:14:20 -0500

    Gettin started

--- graph.cpp
+++ graph.cpp
@@ -2,13 +2,28 @@
 #include <fstream>
 using namespace std;
 
+int nums_nodes;
+int nums_edges;
 
 
 
+void read_file() { // we need to read from the file
+    std::ifstream file ("rmat15.dimacs");
+    // we need to get the edges and the nodes from the file
 
-void read_file() {
+    std::string curWord;
 
-    
+    if (file.is_open()) {
+        while(file.good()) {
+            file >> curWord;
+            std::cout << curWord;
+        }
+    }
 
     return;
+}
+
+
+int main () {
+    read_file();
 }
