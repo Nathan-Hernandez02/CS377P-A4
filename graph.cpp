9198a986555207dbda5ce5b315c83d8fe14c0ec4
Nathan-Hernandez02 <nathannatedoghernandez@gmail.com>
2023-10-23 21:54:36 -0500

    handle dups done

--- graph.cpp
+++ graph.cpp
@@ -7,23 +7,25 @@ using namespace std;
 
 struct node
     {
-        
-        double curNode;
-        double nextNode;
+        double node_label[2];
     };
 
 struct edge { // single edge must have a source, weight, and dest
     int sourceNode;
     int destNode;
     int weight;
-    
+
     edge() {
         sourceNode = 0;
         destNode = 0;
         weight = 0;
     }
     
-    edge(int src, int dest, int whey) : sourceNode(src), destNode(dest), weight(whey) {} // change the formatting on this later
+    edge(int s, int d, int w) {
+        sourceNode = s;
+        destNode = d;
+        weight = w;
+    }
 };
 
 class graph {
@@ -31,7 +33,55 @@ class graph {
         int nums_nodes; // total nodes
         int nums_edges; // total edges
         std::vector<edge> connect; // edge consists of source, destination, and weight
+        struct node * graph_node;
+        int * rp;
+        int * ci;
+        int * ai;
 };
+
+
+
+void handle_arrays(vector<edge> non_dups, graph fin) {
+    int num_edges = non_dups.size() + 2;
+    int num_nodes = fin.nums_nodes + 2;
+
+    fin.rp = new int[num_nodes];
+    fin.ci = new int[num_edges];
+    fin.ai = new int[num_edges];
+    fin.graph_node = new node[num_nodes];
+
+    fin.rp[0] = 1;
+    fin.ci[0] = 1;
+    fin.ai[0] = 1;
+
+    non_dups.insert(non_dups.begin(), edge(fin.rp[0], fin.ci[0], fin.ai[0]));
+
+    int cur_node = 0;
+
+    for(int i = 1; i <= num_edges; i++) {
+        //const?
+        edge cur_edge = non_dups[i];
+        if (cur_node < cur_edge.sourceNode) {
+            cur_node += 1;
+            fin.rp[cur_node] = i;
+        }
+        else{
+            fin.ai[i] = cur_edge.weight;
+            fin.ci[i] = cur_edge.destNode;
+            i++;
+        }
+    }
+
+    int fin_edges = num_edges + 1;
+    int fin_nodes = num_edges + 1;
+
+    for(int i = cur_node + 1; i <= fin_nodes; i++) {
+        fin.rp[i] = fin_edges;
+    }
+
+    fin.ai[fin_edges] = 0;
+    fin.ci[fin_edges] = fin_nodes;
+}
 
 
 vector<edge> sort_graph(graph fin) {
@@ -62,75 +112,30 @@ vector<edge> handle_dups(graph fin) {
             // duplicate edge
             // cout << "yo pierre you wanna come out 'ere" << "\n";
             weight = fin.connect[i].weight > weight ? fin.connect[i].weight : weight;
-        } 
-            else {
-                // cout << "there's no time for hesitation, that only leads to incarceration" << "\n";
-                // add the edge with max_weight to good_edges
-                edge curEdge;
-                curEdge.sourceNode = fin.connect[i - 1].sourceNode;
-                curEdge.destNode = fin.connect[i - 1].destNode;
-                curEdge.weight = weight;
-                            non_dups.push_back(curEdge);
-                            // initialize max_weight
-                weight = fin.connect[i].weight;
-            }
+        } else {
+            // cout << "there's no time for hesitation, that only leads to incarceration" << "\n";
+            // add the edge with max_weight to good_edges
+            edge curEdge;
+            curEdge.sourceNode = fin.connect[i - 1].sourceNode;
+            curEdge.destNode = fin.connect[i - 1].destNode;
+            curEdge.weight = weight;
+            non_dups.push_back(curEdge);
+            // initialize max_weight
+            weight = fin.connect[i].weight;
         }
-        // deal with the last edge
-
+    }
+    // deal with the last edge
     non_dups.push_back(edge(fin.connect[num_edges - 1].sourceNode, fin.connect[num_edges - 1].destNode, weight)); // change this to look like lines 68-64
     
     // cout << " for non dups \n";
-    int x = 0;
-        while (x < non_dups.size()) {
-            cout << non_dups[x].sourceNode << " " << 
-                non_dups[x].destNode << " " << non_dups[x].weight << "\n";
-            x++;
-        }
-
+        // int x = 0;
+        //     while (x < non_dups.size()) {
+        //         cout << non_dups[x].sourceNode << " " << 
+        //             non_dups[x].destNode << " " << non_dups[x].weight << "\n";
+        //         x++;
+        //     }
+    handle_arrays(non_dups, fin);
     return fin.connect;
-    // // get the actual number of edges
-    // num_edges = non_dups.size();
-
-    // // allocate space for dynamic arrays
-    // node = new Node[num_nodes + 2];
-    // rp = new int[num_nodes + 2];
-    // ci = new int[num_edges + 2];
-    // ai = new int[num_edges + 2];
-
-    // // rp[0] = 1 to be consistent with rp[1] = 1 (This affects the number of edges we get for node 1)
-    // rp[0] = 1;
-    // ci[0] = 0;
-    // ai[0] = 0;
-    // good_edges.insert(good_edges.begin(), Edge(rp[0], ci[0], ai[0]));
-
-    // // loop through each edge in COO and construct CSR representation
-    // int cur_node = 0;
-    // int cur_edge = 1;
-    // while (cur_edge <= num_edges)
-    // {
-    //     const Edge &e = good_edges[cur_edge];
-    //     // update cur_node and its coresponding rp when current source node is larger than current node
-    //     if (e.src > cur_node)
-    //     {
-    //         cur_node++;
-    //         rp[cur_node] = cur_edge;
-    //     }
-    //     // copy the destination and weight to ci and ai otherwise
-    //     else
-    //     {
-    //         ci[cur_edge] = e.dst;
-    //         ai[cur_edge] = e.weight;
-    //         cur_edge++;
-    //     }
-    // }
-
-    // // set the remaining nodes without edges
-    // for (int i = cur_node + 1; i <= num_nodes + 1; i++)
-    // {
-    //     rp[i] = num_edges + 1;
-    // }
-    // ci[num_edges + 1] = num_nodes + 1;
-    // ai[num_edges + 1] = 0;
 }
 //
 void read_file(string choice, graph Graph) { // we need to read from the file
@@ -155,47 +160,46 @@ void read_file(string choice, graph Graph) { // we need to read from the file
                 sscanf(curline.c_str(), "a %d %d %d", &curEdge.sourceNode, &curEdge.destNode, &curEdge.weight);
                 
                 fin.connect.push_back(curEdge);
-
-                // cout << curEdge.sourceNode << " " << curEdge.destNode << " " << curEdge.weight << '\n';
             }
         }
         fin.connect = sort_graph(fin);
-        // sort(fin.connect.begin(), fin.connect.end(), [] (const edge& e1, const edge& e2) { 
-            
-        //     if(e1.sourceNode < e2.sourceNode) {
-        //         return true;
-        //     } else if (e1.sourceNode == e2.sourceNode) {
-        //         if(e1.destNode < e2.destNode) {
-        //             return true;
-        //         } else {
-        //             return false;
-        //         } 
-        //     } 
-        //     return false;
-        // });
-
-        // int x = 0;
-        // while (x < fin.connect.size()) {
-        //     cout << fin.connect[x].sourceNode << " " << 
-        //         fin.connect[x].destNode << " " << fin.connect[x].weight << "\n";
-        //     x++;
-        // }
-
         fin.connect = handle_dups(fin);
     }
 
     return;
 }
 
-// void pagerank(graph graph){
+void pagerank(graph graph){
+    double thresh_hold = 1.0e-4;
+    double damping = 0.85;
 
-//     double thresh_hold = 1.0e-4;
-//     double damping = 0.85;
+    int num_nodes = graph.nums_nodes;
+    int num_edges = graph.nums_edges;
+    vector<edge> current = graph.connect;
 
-//     int num_nodes = graph.nums_nodes;
-//     int num_edges = graph.nums_edges;
-//     vector<edges> current = graph.connect;
-// }
+    bool is_converged = false;
+    bool first_time = true;
+
+    while (!is_converged || first_time) {
+
+        // reset_next_level
+
+        for (int i = 0; i < num_nodes; i++) {
+        }
+
+        for (int s = 1; s < num_nodes; s++) {
+            // get label
+            // get label
+            // if
+                // false
+            // else
+                // true
+        }
+
+
+        first_time = false;
+    }
+}
 
 int main (int argc, char* argv[]) {
 
