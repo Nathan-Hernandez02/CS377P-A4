595f03f2239839f5c5851faa82b0086520ca28a9
Nathan Hernandez <nathannatedoghernandez@gmail.com>
2023-10-25 19:12:20 -0500

    histograms

--- histo.py
+++ histo.py
@@ -1,26 +1,82 @@
+import networkx as nx
 import matplotlib.pyplot as plt
+my_list = []
+my_nums = []
+# Function to read a DIMACS file and create a NetworkX graph
+def readDIMACSFile(filename):
+    global my_list
+    global my_nums
+    G = nx.Graph()
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('c'):
+                # Skip comments
+                continue
+            elif line.startswith('p'):
+                _, _, numVertices, numEdges = line.split()
+                my_list = [0 for i in range(int(numVertices))] 
+                my_nums = [i for i in range(int(numVertices))] 
+                # You can process 'numVertices' and 'numEdges' as needed.
+            elif line.startswith('a'):
+                _, nodeA, nodeB, _ = line.split()
+                G.add_edge(int(nodeA), int(nodeB))
+                my_list[int(nodeA) - 1] =  my_list[int(nodeA) - 1] + 1
+                # For an undirected graph, you may also need to add the reverse edge:
+                # G.add_edge(int(nodeB), int(nodeA))
+    return G
 
-# Read the data from the text file
-data = []
-with open('histo_wiki.dimacs.txt', 'r') as file:
-    for line in file:
-        parts = line.split()
-        if len(parts) == 5:
-            node_id = int(parts[1])
-            num_outgoing_edges = int(parts[4])
-            data.append((node_id, num_outgoing_edges))
+# Function to read weights from a separate file
+def readWeightsFile(filename):
+    node_weights = {}
+    with open(filename, 'r') as file:
+        for line in file:
+            _, node, _, weight = line.split()
+            node_weights[int(node)] = float(weight)
+    return node_weights
 
-# Separate the data into node IDs and the number of outgoing edges
-node_ids, outgoing_edges = zip(*data)
+# Function to visualize the graph with node sizes based on weights
+def visualizeGraph(graph, node_weights, weights_filename):
+    pos = nx.planar_layout(graph)  # You can use different layouts here
 
-# Create a bar chart
-plt.bar(node_ids, outgoing_edges, color='skyblue')
-plt.xlabel('Node ID')
-plt.ylabel('Number of Outgoing Edges')
-plt.title('Outgoing Edges Histogram')
+    # Scale the node sizes based on weights
+    scaled_sizes = [50000 * node_weights[node] for node in graph.nodes]
+    node_colors = [node_weights[node]  for node in graph.nodes]
+    # node_cmap = plt.cm.viridis
+    node_labels = {}
+    with open(weights_filename, "r") as file:
+        for line in file:
+            node_id, label = line.strip().split()
+            node_labels[int(node_id)] = label
 
-# Save the plot to a file (e.g., outgoing_edges.png)
-plt.savefig('outgoing_edges.png')
+    plt.figure(figsize=(10, 6))
+    nx.draw(graph, pos, node_size=scaled_sizes, with_labels=False, font_size=10, font_color='black', node_color=node_colors, edge_color='black')
+    nx.draw_networkx_labels(G, pos, node_labels, font_size=10, font_color='black')
 
-# Show the plot (optional)
-plt.show()
+    plt.title(f'Graph Visualization with Node Sizes Based on Weights')
+    plt.show()
+
+# Function to create a histogram of out-degrees
+def createOutDegreeHistogram():
+    global my_list
+    global my_nums
+    plt.bar(my_nums, my_list, label="data")
+    plt.xlabel('Vertex')
+    plt.ylabel('Number of Nodes')
+    plt.title('Outgoing edges')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
+
+if __name__ == "__main__":
+    filename = "wiki.dimacs"  # Change this to your DIMACS file
+    weights_filename = "pagerank_wiki.dimacs"  # Change this to the separate weights file
+    G = readDIMACSFile(filename)
+    node_weights = readWeightsFile(weights_filename)
+
+    # Visualize the graph with node sizes based on weights
+    visualizeGraph(G, node_weights, weights_filename)
+
+    # Create a histogram of out-degrees
+    createOutDegreeHistogram()
+
+    print("Graph visualized with node sizes based on weights, and an out-degree histogram using NetworkX and Matplotlib.")
